ce500bd5bdc241ff5b16a4e5861988722d82f852
J3fr3Y <jefreysanchez888@gmail.com>
2026-04-16 15:52:17 +0200

    actual moving code

--- testMov/testMov.ino
+++ testMov/testMov.ino
@@ -1,30 +1,38 @@
 #include <Arduino.h>
-// DFRobot L293 Shield V1.1
-// Motor 1: PWM D6, DIR D7
-// Motor 2: PWM D5, DIR D4
 
 const int M1_PWM = 6;
 const int M1_DIR = 7;
 const int M2_PWM = 5;
 const int M2_DIR = 4;
 
-void setup() {
-  pinMode(M1_PWM, OUTPUT);
-  pinMode(M1_DIR, OUTPUT);
-  pinMode(M2_PWM, OUTPUT);
-  pinMode(M2_DIR, OUTPUT);
+const int CRUISE1 = 255;   // tune separately 6.57 v for the left motor
+const int CRUISE2 = 255;  // tune separately
+const int KICK = 160;     // startup kick
+const int KICK_TIME = 150;
 
-  Serial.begin(9600);
+void setup() {
+  pinMode(M1_DIR, OUTPUT);
+  pinMode(M1_PWM, OUTPUT);
+  pinMode(M2_DIR, OUTPUT);
+  pinMode(M2_PWM, OUTPUT);
+
+  // Force everything off immediately
+  digitalWrite(M1_DIR, LOW);
+  digitalWrite(M2_DIR, LOW);
+  analogWrite(M1_PWM, 0);
+  analogWrite(M2_PWM, 0);
+
+  delay(200);
 }
 
 void motor1(int speedVal, bool forward) {
   digitalWrite(M1_DIR, forward ? HIGH : LOW);
-  analogWrite(M1_PWM, speedVal);   // 0..255
+  analogWrite(M1_PWM, speedVal);
 }
 
 void motor2(int speedVal, bool forward) {
   digitalWrite(M2_DIR, forward ? HIGH : LOW);
-  analogWrite(M2_PWM, speedVal);   // 0..255
+  analogWrite(M2_PWM, speedVal);
 }
 
 void stopMotors() {
@@ -32,18 +40,26 @@ void stopMotors() {
   analogWrite(M2_PWM, 0);
 }
 
+void drive(bool forward, int s1, int s2) {
+  // startup kick to overcome static friction
+  motor1(KICK, forward);
+  motor2(KICK, forward);
+  delay(KICK_TIME);
+
+  motor1(s1, forward);
+  motor2(s2, forward);
+}
+
 void loop() {
-  Serial.println("Forward");
-  motor1(90, true);
-  motor2(90, true);
-  delay(3000);
+  drive(true, CRUISE1, CRUISE2);
+  delay(2000);
 
-  Serial.println("Backward");
-  motor1(90, false);
-  motor2(90, false);
-  delay(3000);
+  stopMotors();
+  delay(2000);
 
-  Serial.println("Stop");
+  drive(false, CRUISE1, CRUISE2);
+  delay(2000);
+
   stopMotors();
   delay(2000);
 }
