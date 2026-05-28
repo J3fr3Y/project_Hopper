577e4e08ffbf0c5a3a5cde66377d7d007662ab27
J3fr3Y <jefreysanchez888@gmail.com>
2026-05-28 18:42:02 +0200

    Correction des pins des capteurs, LES CAPTEURS MARCHENTgit add .

--- main/main.ino
+++ main/main.ino
@@ -8,7 +8,7 @@
 #define M2_PWM 5
 #define M2_DIR 4
 #define nb_cptr 3
-#define PIN_CAPTEURS {2,3,4}
+#define PIN_CAPTEURS {2,3,8}//change le pin du capteur 4 a 8 pour pas de conflit avec le pin de dir du moteur 2
 
 const int CRUISE1 = 120;
 const int CRUISE2 = 124;
@@ -16,7 +16,11 @@ const int CRUISE2 = 124;
 QTRSensors capteur;//installer la librairie QTRSensors de Pololu (oui c'est le bon nom)
 //Optimal sensing distance: 0.125" (3 mm)
 //Maximum recommended sensing distance: 0.25" (6 mm)
+//on met le capteur comme digital
+//set
 
+
+//on met
 void setup() {
     Serial.begin(9600);
     uint8_t pinsCapteurs[] = PIN_CAPTEURS;
@@ -33,8 +37,13 @@ void setup() {
 }
 
 void loop() {
-    suivreLigne(capteur);
-
+    //suivreLigne(capteur);
+    //QTRType typeCapteur = capteur.getType();
+    /*if(typeCapteur == QTRType::RC){
+        Serial.println("Capteur est RC(digital)");
+    } else if(typeCapteur == QTRType::Analog){
+        Serial.println("Capteur est analogique");
+    }*/
     /*uint16_t sensorsCali[nb_cptr];
     capteur.readCalibrated(sensorsCali);
 
@@ -46,7 +55,7 @@ void loop() {
     delay(500);*/
 
     //avancer(CRUISE1,CRUISE2);
-
+    suivreLigne(capteur);
     
 }
 
